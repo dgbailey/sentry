8ad3113403e678c2ef4548fd0fb56a4ad4c20e25
dgbailey <bailey.dust@gmail.com>
2020-09-04 16:25:09 -0700

    Changes in order to serve properly before source file upload configured.

--- custom-webpack.config.js
+++ custom-webpack.config.js
@@ -3,14 +3,14 @@ const SentryWebpackPlugin = require("@sentry/webpack-plugin");
 module.exports = {
   // other configuration
  
-    plugins: [
-      new SentryWebpackPlugin({
-        filenameTransform: filename => '/' + filename,
-        include:"./dist/sentry-exercise",
-        ignoreFile: ".sentrycliignore",
-        ignore: ["node_modules", "webpack.config.js"],
-        configFile: "sentry.properties",
-      }),
-    ],
+    // plugins: [
+    //   new SentryWebpackPlugin({
+    //     filenameTransform: filename => '/' + filename,
+    //     include:"./dist/sentry-exercise",
+    //     ignoreFile: ".sentrycliignore",
+    //     ignore: ["node_modules", "webpack.config.js"],
+    //     configFile: "sentry.properties",
+    //   }),
+    // ],
   
 };
